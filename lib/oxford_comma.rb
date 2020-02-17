d759fd18cf4b0a2239df8ab026b175b244ff91da
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-17 06:43:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
       array.join
     elsif array.length == 2
       array.insert(1, "and").join(" ")
-    else str = ", and" + array.pop
+    else str = ", and " + array.pop
       str = array.join(",") << str
     end
 end
